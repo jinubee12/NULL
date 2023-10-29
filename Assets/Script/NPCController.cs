1fb339d61910aa3b1bd553e9b5ea8fddce91f67b
jinubee12 <jinubee12@naver.com>
2023-10-29 19:48:30 +0900

    Get Start
    유니티 게임 플레이어와 NPC 기본 collider 구현

---
+++ Assets/Script/NPCController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour, Interactable
+{
+    public void interact()
+    {
+        Debug.Log("You will talk to this NPC");
+    }
+}
