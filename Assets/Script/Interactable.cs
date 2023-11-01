aa2e4866a537aa9ccc446e261547f80109c52158
jinubee12 <91825408+jinubee12@users.noreply.github.com>
2023-11-02 01:53:42 +0900

    Delete Assets directory

--- Assets/Script/Interactable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Interactable 
-{ 
-    // Start is called before the first frame update
-    void interact();
-}
